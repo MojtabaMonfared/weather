38501777dc3f3b9bbd6510eee27dc7a25ff6f4ae
MojtabaMonfared <mojmon42@gmail.com>
2022-01-03 21:19:37 +0330

    testing autocomplete using jquery

--- webapp/api.py
+++ webapp/api.py
@@ -4,11 +4,12 @@ import json
 API_KEY = '888a0ae798984bd699191644220101'
 
 def get_current_weather(query):
-    CURRENT_URL = 'https://api.weatherapi.com/v1/current.json'
+    CURRENT_URL = 'https://api.weatherapi.com/v1/search.json'
     parameters = {
         'key': API_KEY,
         'q': query,
     }
     r = requests.get(CURRENT_URL, params=parameters)
     result_weather = json.loads(r.text)
-    return result_weather
+    for i in result_weather:
+        return i['name']
--- webapp/views.py
+++ webapp/views.py
@@ -4,6 +4,33 @@ from .api import *
 from .forms import CityName
 
 
+# def get_data_by_city(request):
+#     form = CityName(request.POST or None)
+#     if form.is_valid():
+#         json_data = get_current_weather(form.cleaned_data['city'])
+#         if 'error' in json_data:
+#             context = {
+#                 'error': json_data['error']['message'],
+#                 'wrong_name': form.data['city'],
+#             }
+#             return render(request, 'webapp/data.html', context)
+#         else:
+#             context = {
+#                 'city': json_data['location']['name'],
+#                 'country': json_data['location']['country'],
+#                 'temp': json_data['current']['temp_c'],
+#                 'temp_f': json_data['current']['temp_f'],
+#                 'condition': json_data['current']['condition']['text'],
+#                 'icon': json_data['current']['condition']['icon'],
+#                 'wind_kph': json_data['current']['wind_kph'],
+#                 'humidity': json_data['current']['humidity'],
+#                 'lat': json_data['location']['lat'],
+#                 'lon': json_data['location']['lon'],
+#                 }
+#             # return render(request, 'webapp/data.html', context)
+#             return render(request, 'webapp/result.html', context)
+#     return render(request, 'webapp/data.html', {'form': form})
+
 def get_data_by_city(request):
     form = CityName(request.POST or None)
     if form.is_valid():
@@ -16,17 +43,8 @@ def get_data_by_city(request):
             return render(request, 'webapp/data.html', context)
         else:
             context = {
-                'city': json_data['location']['name'],
-                'country': json_data['location']['country'],
-                'temp': json_data['current']['temp_c'],
-                'temp_f': json_data['current']['temp_f'],
-                'condition': json_data['current']['condition']['text'],
-                'icon': json_data['current']['condition']['icon'],
-                'wind_kph': json_data['current']['wind_kph'],
-                'humidity': json_data['current']['humidity'],
-                'lat': json_data['location']['lat'],
-                'lon': json_data['location']['lon'],
-                }
+                'cities': json_data,
+            }
             # return render(request, 'webapp/data.html', context)
             return render(request, 'webapp/result.html', context)
     return render(request, 'webapp/data.html', {'form': form})
